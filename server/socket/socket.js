cf4dcfc685744935c3d3c84433a25c04bcfabdb5
rajesh k <143821418+Rajeshstark2@users.noreply.github.com>
2025-02-23 19:53:58 +0530

    Update socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://chatemate1.netlify.app"],
+		origin: ["*"],
 		methods: ["GET", "POST"],
 	},
 });
